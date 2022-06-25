d9a4319a71aa9a70a6f7d87913019ac38246c39e
w_sarmiento_ <98241508+WaltherInc@users.noreply.github.com>
2022-06-25 11:21:36 -0500

    LoginIn - Password and Usuary

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  durationInSeconds = 5;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  form: FormGroup
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      usuary: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+   }
 
   ngOnInit(): void {
   }
 
-}
+  Login() {
+    console.log(this.form);
+    const usuary = this.form.value.usuary;
+    const password = this.form.value.password;
 
-export class FormFieldPrefixSuffixExample {
-  hide = true;
-}
+    console.log(usuary);
+    console.log(password);
+
+    if(usuary == 'admin@company.com' && password == 'admin'){
+      //Enter to the dashboard
+    } else {
+      //show error
+      this.openSnackBar()
+    }
+  }
+    openSnackBar() {
+      this._snackBar.open('Incorrect username or password', 'Splash', {
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+        duration: this.durationInSeconds * 1000,
+      });
+    }
+  }
+
 
