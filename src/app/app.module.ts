668e877abab41268d69c59019a8d3939ee13bc39
w_sarmiento_ <98241508+WaltherInc@users.noreply.github.com>
2022-06-28 14:40:03 -0500

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-
-//Angular material
-import { MatSliderModule } from '@angular/material/slider';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { SharedModule } from './components/shared/shared.module';
 
 //Components
 import { LoginComponent } from './components/login/login.component';
@@ -26,13 +18,8 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    MatSnackBarModule,
-    MatProgressSpinnerModule,
+    SharedModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
